e961c0a4f47ca21377df860b48b7415ae50b1b99
Zura Guerra <guerrazuramx@gmail.com>
2016-03-12 16:21:04 -0600

    Support negative and positive numbers

--- lib/poliwave/encoder.ex
+++ lib/poliwave/encoder.ex
@@ -1,23 +1,30 @@
 defmodule Poliwave.Encoder do
   use Bitwise
   
-  def encode(num) do
-    num
-    |> round
-    |> to_binary
+  def encode(number) do
+    sign_and_number = number
+                    |> round
+                    |> to_binary
+
+    case sign_and_number do
+      {:negative, number} ->
+      {:positive, number} ->
+    end
   end
 
-  defp round(num) do
-    num = num * 1.0e5
-        |> round
+  defp round(number) do
+    number = number * 1.0e5
+           |> round
   end
 
-  defp to_binary(num) do
-    if num < 0 do
-      num = num * -1
-          |> Integer.digits(2)
+  defp to_binary(number) do
+    if number < 0 do
+      number = number * -1
+             |> Integer.digits(2)
+      {:negative, number}
     else
-      Integer.digits(num, 2)
+      Integer.digits(number, 2)
+      {:positive, number}
     end
   end
 end
