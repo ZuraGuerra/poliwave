6c4cac97c8b55eca34a69f32e5a8bb3bccdd9d04
Zura Guerra <guerrazuramx@gmail.com>
2016-03-12 16:15:40 -0600

    Convert to binary

--- lib/poliwave/encoder.ex
+++ lib/poliwave/encoder.ex
@@ -4,10 +4,20 @@ defmodule Poliwave.Encoder do
   def encode(num) do
     num
     |> round
+    |> to_binary
   end
 
   defp round(num) do
     num = num * 1.0e5
         |> round
+  end
+
+  defp to_binary(num) do
+    if num < 0 do
+      num = num * -1
+          |> Integer.digits(2)
+    else
+      Integer.digits(num, 2)
+    end
   end
 end
